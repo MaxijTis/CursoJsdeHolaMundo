ce0d91dea1118193b1c788e35b206814de4251fc
Maximiliano Tis <maxi_tis@hotmail.com>
2024-03-31 17:47:59 -0300

    Comentarios y Objetos

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,17 @@
+/*
+Esta es la manera oficial
+de hacer
+comentarios 
+multilineas
+*/
+
+
+/**
+ * Este es un Comentario
+ * de
+ * Muchas lineas
+ */
+
+let numero = 42; //Este es otro comentario
+
+console.log(numero);
