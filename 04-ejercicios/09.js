84585202d4220827e8b6832fe0ea6f2f57e01460
Maximiliano Tis <maxi_tis@hotmail.com>
2024-04-02 02:14:19 -0300

    Finalizando los ultimos ejercicios

---
+++ 04-ejercicios/09.js
@@ -0,0 +1,33 @@
+//crea un algoritmo que devuelva un
+//array de objetos en base a pares
+
+let pairs =[
+    [1, {name: "Nicolas"}],
+    [2, {name: "Felipe"}],
+    [3, {name: "Chanchito"}],
+];
+
+let array = [{
+    id: 1,
+    name: 'Nicolas',
+},{
+    id: 2,
+    name: 'Felipe',
+},{
+    id:3,
+    name: 'Chanchito',
+}];
+
+function toCollection(arr){
+    let collection = [];
+    for (idx in arr){
+        let elemento = arr[idx];
+        collection[idx] = elemento[1];
+        collection[idx].id = elemento[0];
+    }
+    
+    return collection;
+}
+
+let resultado = toCollection(pairs);
+console.log(resultado);
